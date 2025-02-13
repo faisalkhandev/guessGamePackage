896d27bee43ced79a217037265a3fc315f805760
faisalKhan <faisaldev404@gmail.com>
2025-02-13 17:25:14 +0500

    add: user can input any number from 1 to 10 and

---
+++ guess.js
@@ -0,0 +1,26 @@
+const { program } = require("commander");
+
+program
+    .option('-g, --guess <guess>', 'Your guess number');
+
+program.parse(); // Takes input from the user
+
+const readUserInput = program.opts();
+
+const userGuess = parseInt(readUserInput.guess); // Convert input to a number
+
+const generateRandomNumber = Math.floor(Math.random() * 10) + 1; // Random number between 1-10
+
+if (isNaN(userGuess) || userGuess < 1 || userGuess > 10) {
+    console.log("❌ Please enter a **valid number** between 1 and 10!");
+    process.exit(1);
+}
+
+if (userGuess === generateRandomNumber) {
+    console.log("🎉 Correct! You guessed the right number!");
+} else {
+    console.log(`❌ Wrong guess! The correct number was ${generateRandomNumber}.`);
+}
+
+
+console.log("developed by @faisalkhandev")
